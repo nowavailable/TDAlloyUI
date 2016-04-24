2b85903f31601b0a22c5a3cf1f78c70a404b304d
nowavailable <taiga.tsutsumi@gmail.com>
2016-04-24 18:53:24 +0900

    Using ES2015 polyphill for userland of electron and transpile it by gulp.

---
+++ src/main_electron.js
@@ -0,0 +1,54 @@
+'use strict';
+
+require("babel-polyfill");
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
+import { Router, useRouterHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import createStore from './store/createStore'
+import { Provider } from 'react-redux'
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 1300, height: 840});
+
+  mainWindow.loadURL('file://' + __dirname + '/index_electron.html');
+
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+
+
+});
